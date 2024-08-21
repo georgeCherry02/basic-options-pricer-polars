0243019f8aa66ecc9ac7df236288166e8864136e
GeorgeCherry02 <georgeb.cherry@gmail.com>
2024-08-21 18:28:51 +0100

    Add test for kwarg binding usage

--- pricer_polars/bindings.py
+++ pricer_polars/bindings.py
@@ -1,6 +1,9 @@
 from polars.plugins import register_plugin_function
 from pathlib import Path
 
+from datetime import datetime
+import pytz
+
 import polars as pl
 
 def calculate_black_scholes_explicit(*expr):
@@ -9,5 +12,19 @@ def calculate_black_scholes_explicit(*expr):
         plugin_path=Path(__file__).parent,
         args=expressions,
         function_name="calculate_black_scholes_explicit",
+        is_elementwise=True,
+    )
+
+def calculate_black_scholes(*expr, strike: float, risk_free_rate: float, expiry: datetime):
+    expressions = [pl.col(exp) for exp in expr]
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        args=expressions,
+        kwargs={
+            "strike": strike,
+            "risk_free_rate": risk_free_rate,
+            "expiry": pytz.utc.localize(expiry).isoformat(),
+        },
+        function_name="calculate_black_scholes",
         is_elementwise=True,
     )
--- tests/test_polars_black_scholes.py
+++ tests/test_polars_black_scholes.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-from pricer_polars.bindings import calculate_black_scholes_explicit
+from pricer_polars.bindings import calculate_black_scholes, calculate_black_scholes_explicit
 
 import polars as pl
 
@@ -24,5 +24,19 @@ def test_polars_integration():
         black_scholes = calculate_black_scholes_explicit("strike", "volatility", "underlying", "rfr", "expiry")
     )
     value = out.row(1, named=True)["black_scholes"]
+    assert is_close(value, 2.0557, 0.0001), "Valued 4 month call correctly"
+
+def test_polars_kwargs_integration():
+    strike = 45.0
+    risk_free_rate = 0.04
+    expiry = in_four_months()
+    df = pl.DataFrame({
+        "underlying": [40.0, 45.0, 50.0],
+        "volatility": [0.4] * 3,
+    })
+    out = df.with_columns(
+        black_scholes = calculate_black_scholes("underlying", "volatility", strike=strike, risk_free_rate=risk_free_rate, expiry=expiry)
+    )
+    value = out.row(0, named=True)["black_scholes"]
     assert is_close(value, 2.0557, 0.0001), "Valued 4 month call correctly"
 
