24d60b3abcc8b8e684db9b908c795df542102e91
GeorgeCherry02 <georgeb.cherry@gmail.com>
2024-08-23 10:52:27 +0100

    Flatten multizip

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use pricer::{option::get_call, BlackScholes};
 use chrono::prelude::Utc;
 use chrono::DateTime;
 
+use itertools::multizip;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -36,31 +37,36 @@ fn calculate_black_scholes(inputs: &[Series], kwargs: BlackScholesKwargs) -> Pol
     Ok(out.into_series())
 }
 
+fn bind_optionals_to_black_scholes(
+    (strike, volatility, underlying, risk_free_rate, expiry): (
+        Option<f64>,
+        Option<f64>,
+        Option<f64>,
+        Option<f64>,
+        Option<i64>,
+    ),
+) -> Option<f64> {
+    let strike = strike?;
+    let volatility = volatility?;
+    let underlying = underlying?;
+    let risk_free_rate = risk_free_rate?;
+    let expiry = expiry.and_then(|e| DateTime::from_timestamp_micros(e))?;
+    let call = get_call(strike, expiry);
+    call.value_black_scholes(Utc::now(), volatility, underlying, risk_free_rate)
+        .ok()
+}
+
 #[polars_expr(output_type=Float64)]
 fn calculate_black_scholes_explicit(inputs: &[Series]) -> PolarsResult<Series> {
-    let strike = inputs[0].f64()?;
-    let volatility = inputs[1].f64()?;
-    let underlying = inputs[2].f64()?;
-    let rfr = inputs[3].f64()?;
-    let expiry = inputs[4].datetime()?;
-    let out: ChunkedArray<Float64Type> = strike
-        .into_iter()
-        .zip(volatility.into_iter())
-        .zip(underlying.into_iter())
-        .zip(rfr.into_iter())
-        .zip(expiry.into_iter())
-        .map(
-            |((((strike, volatility), underlying), rfr), expiry)| -> Option<f64> {
-                let strike = strike?;
-                let volatility = volatility?;
-                let underlying = underlying?;
-                let rfr = rfr?;
-                let expiry = DateTime::from_timestamp_micros(expiry?)?;
-                let call = get_call(strike, expiry);
-                call.value_black_scholes(Utc::now(), volatility, underlying, rfr)
-                    .ok()
-            },
-        )
+    let bound_tuple = (
+        inputs[0].f64()?.into_iter(),
+        inputs[1].f64()?.into_iter(),
+        inputs[2].f64()?.into_iter(),
+        inputs[3].f64()?.into_iter(),
+        inputs[4].datetime()?.into_iter(),
+    );
+    let out: ChunkedArray<Float64Type> = multizip(bound_tuple)
+        .map(bind_optionals_to_black_scholes)
         .collect();
     Ok(out.with_name("black_scholes").into_series())
 }
